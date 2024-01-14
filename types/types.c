269b59df1f693fadec69e40246f43ffba56f81e6
Adonis Cruz <adonisacruz0@gmail.com>
2024-01-14 11:58:19 -0500

    basic C types

---
+++ types/types.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  char letter = 'A';
+  char string[] = "Hello world";
+  int one = 1;
+  long number = 123456789;
+
+  printf("Char: %c\n", letter);
+  printf("String: %s\n", string);
+  printf("Int: %d\n", one);
+  printf("Long: %ld\n", number);
+
+  return 0;
+}
