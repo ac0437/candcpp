1761dc039661b95d2cbf65615f23976642829b2d
Adonis Cruz <adonisacruz0@gmail.com>
2024-01-14 12:22:06 -0500

    Adding double vs float, bloolean, and other examples

--- types/types.c
+++ types/types.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
   char letter = 'A';
-  char string[] = "Hello world";
+  char string[] = "Hello world!";
+  char stringEscape[] = "\"Need \\ to escape \"";
   int one = 1;
+  double decimalNumber = 5.5;
+  float decimalFloat = 5.5f;
   long number = 123456789;
+  bool truthy = true;
 
   printf("Char: %c\n", letter);
   printf("String: %s\n", string);
-  printf("Int: %d\n", one);
+  printf("String escape: %s\n", stringEscape);
+  printf("Int: %i\n", one);
+  printf("Dobule: %lf\n", decimalNumber);
+  printf("Float: %f\n", decimalFloat);
   printf("Long: %ld\n", number);
+  printf("Boolean: %i\n", truthy);
 
   return 0;
 }
--- types/types.md
+++ types/types.md
@@ -3,9 +3,44 @@
 ### Char
 
 `ASCII characters`: Mapping of letter to number
+uses single quotes
+print with %c
 
 #### [Unicode](https://home.unicode.org/)
 
 larger mapping that can be used with `\u<unicode_number>`
 
 ### Hexidecimal
+
+### String
+
+represented by a `char string[] = "String";`
+use double quotes
+print with %s
+
+### Int
+
+`int number = 1;`
+print with %i or %d
+
+### Double
+
+`double myDouble = 5.5;`
+perferred over floats
+print with %f
+
+### Float
+
+`float myFloat = 5.5f;`
+less percision then a double
+print with %f
+
+### Long
+
+`long longNumber = 123456789;`
+print with %ld
+
+### Boolean
+
+`bool myBool = true`
+print with %i or %d
